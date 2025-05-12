604a742dc7454b208bfa2e948109f0976fc30ed2
Fraga <gustavohfraga1@gmail.com>
2025-05-12 20:57:24 -0300

    header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -6,19 +6,20 @@ const Header = () => {
       <div className="mx-auto flex max-w-7xl items-center justify-between px-8 py-4">
         <div className="flex items-center">
           <img
-            className="h-10"
-            src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
-            alt="Logo da Hashtag"
+            className="h-15"
+            src="https://www.diplomatafm.com.br/wp-content/uploads/2021/09/logo-acapra.png"
+            alt="Logo Acapra"
           />
-          <p className="text-primary-400 text-2xl font-bold">ashbnb</p>
+
+          
+      
         </div>
 
         <div className="flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md">
-          <p className="border-r border-r-gray-300 pr-4">Qualquer lugar</p>
-          <p className="border-r border-r-gray-300 px-4">Qualquer semana</p>
-          <p className="px-4">Hóspedes</p>
+          <p className="border-r border-r-gray-300 pr-4">Encontre seu novo melhor amigo para adoção</p>
+        
 
-          <div className="bg-primary-400 rounded-full p-2 text-white">
+          <div className="bg-primary-400 rounded-full p-1 text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -63,7 +64,7 @@ const Header = () => {
             />
           </svg>
 
-          <p>Diego XYZ</p>
+          <p>Fraguinha</p>
         </div>
       </div>
     </div>
